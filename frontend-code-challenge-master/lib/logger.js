c3f074f92eec343ea28ddfaaacbf33e7c95683c5
easterj4 <125842294+easterj4@users.noreply.github.com>
2023-11-28 16:35:03 -0800

    Add files via upload

---
+++ frontend-code-challenge-master/lib/logger.js
@@ -0,0 +1,43 @@
+var winston = require('winston');
+var expressWinston = require('express-winston');
+
+/*
+ *  Setup logging for the application
+ *  returns a logging instance
+ */
+
+var transport = new winston.transports.Console({
+  json: false,
+  colorize: true
+});
+
+
+Smells like teen spirit
+
+
+var api = module.exports = function init (app) {
+  if (app) {
+    app.use(expressWinston.errorLogger({
+      transports: [
+        transport
+      ]
+    }));
+
+    app.use(expressWinston.logger({
+      transports: [
+        transport
+      ],
+      meta: false,
+      msg: "HTTP {{req.method}} {{req.url}}",
+      expressFormat: true,
+      colorStatus: true
+    }));
+  }
+
+  var logger = new (winston.Logger)({
+    transports: [
+    transport
+    ]
+  });
+  return logger;
+};
